e2b7c82b5c2f067da261efe886f89daff675a9ec
Jayaram Mani <jayaram.mani@optum.com>
2019-04-23 23:36:06 +0100

    Ajax jquery

--- FitnessTracker/src/main/java/com/springmvc/controller/MinutesController.java
+++ FitnessTracker/src/main/java/com/springmvc/controller/MinutesController.java
@@ -1,27 +1,48 @@
 package com.springmvc.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.springmvc.model.Activity;
 import com.springmvc.model.Exercise;
+import com.springmvc.service.ExerciseService;
 
 @Controller
 public class MinutesController {
+	
+	@Autowired
+	private ExerciseService exerciseService;
 
 	@RequestMapping(value = "/addminutes")
 	public String addMinutes(@ModelAttribute ("exercise") Exercise exercise) {
 		
 		System.out.println("minutes " + exercise.getMinutes());
+		System.out.println("activity " + exercise.getActivity());
 		
-		return "redirect:addmoreminutes.html";
-	}
-	
-	@RequestMapping(value = "/addmoreminutes")
-	public String addMoreMinutes(@ModelAttribute ("exercise") Exercise exercise) {
-		
-		System.out.println("more minutes " + exercise.getMinutes());
-		
+		// return "redirect:addmoreminutes.html";
 		return "addMinutes";
 	}
+	
+//	@RequestMapping(value = "/addmoreminutes")
+//	public String addMoreMinutes(@ModelAttribute ("exercise") Exercise exercise) {
+//		
+//		System.out.println("more minutes " + exercise.getMinutes());
+//		
+//		return "addMinutes";
+//	}
+	
+	
+	@RequestMapping(value = "/activities", method = RequestMethod.GET)
+	public @ResponseBody List<Activity> findAllActivities() {
+		
+		return exerciseService.findAllActivities();
+		
+	}
+	
 }
--- FitnessTracker/target/FitnessTracker/WEB-INF/jsp/addGoal.jsp
+++ FitnessTracker/target/FitnessTracker/WEB-INF/jsp/addGoal.jsp
@@ -7,18 +7,33 @@
 <head>
 <meta charset="UTF-8">
 <title>Add Goal</title>
+
+<style>
+	.error {
+		color: #ff0000;
+	}
+	.errorblock {
+		color: #000
+		background-color: #ffeeee;
+		border: 3px solid #ff0000;
+		padding: 0px;
+		margin: 16px;
+	}
+</style>
+
 </head>
 <body>
 
 <form:form modelAttribute="goal">
-
+	<form:errors path="*" cssClass="errorblock" element="div"/>
 	<table>
 		<tr>
 			<td>Enter Minutes:</td>
-			<td><form:input path="minutes"/></td>
+			<td><form:input path="minutes" cssErrorClass="error"/></td>
+			<td><form:errors path="minutes" cssClass="error"/></td>
 		</tr>
 		<tr>
-			<td colspan="2">
+			<td colspan="3">
 				<input type="submit" value="Enter Goal Minutes"/>
 			</td>
 		</tr>
